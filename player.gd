0daab866c06ee45e554e6872b07d526d9162c61d
aniya <fyeasmin1@fordham.edu>
2025-05-07 15:50:09 -0400

    mouse motion w camera

--- player.gd
+++ player.gd
@@ -1,5 +1,11 @@
 extends RigidBody3D
 
+var mouse_sensitivity := 0.001
+var twist_input := 0.0
+var pitch_input := 0.0
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta: float) -> void:
 	var input := Vector3.ZERO
@@ -7,3 +13,15 @@ func _process(delta: float) -> void:
 	input.z = Input.get_axis("move_forward", "move_down")
 	
 	apply_central_force(input * 1200.0 * delta)
+	
+	#mouse disapear when playing
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	$twistpivot.rotate_y(twist_input)
+		
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensitivity
+			pitch_input = - event.relative.y * mouse_sensitivity
