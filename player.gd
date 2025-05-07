6f65871e37c1567462465f82100a5e5d3afa2a73
aniya <fyeasmin1@fordham.edu>
2025-05-07 15:30:46 -0400

    camera + player movement

---
+++ player.gd
@@ -0,0 +1,9 @@
+extends RigidBody3D
+
+
+func _process(delta: float) -> void:
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_down")
+	
+	apply_central_force(input * 1200.0 * delta)
