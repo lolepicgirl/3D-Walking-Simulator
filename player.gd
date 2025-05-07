3bca258533ca0673686401afcf350871055ddacc
aniya <fyeasmin1@fordham.edu>
2025-05-07 16:11:00 -0400

    camera

--- player.gd
+++ player.gd
@@ -4,6 +4,10 @@ var mouse_sensitivity := 0.001
 var twist_input := 0.0
 var pitch_input := 0.0
 
+@onready var twist_pivot := $twistpivot
+@onready var pitch_pivot := $twistpivot/pitchpivot
+
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -12,19 +16,15 @@ func _process(delta: float) -> void:
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_down")
 	
-	apply_central_force(input * 1200.0 * delta)
+	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
 	
 	#mouse disapear when playing
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
-	$twistpivot.rotate_y(twist_input)
-	$twistpivot/pitchpivot.rotate_x(pitch_input)
-	$twistpivot/pitchpivot.rotation.x = clamp(
-		$twistpivot/pitchpivot.rotation.x,
-		-0.5,
-		0.5
-	)
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, deg_to_rad(-30), deg_to_rad(30))
 	twist_input = 0.0
 	pitch_input = 0.0
 
